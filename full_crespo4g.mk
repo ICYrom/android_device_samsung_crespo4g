39df57651ee147e1007533b90770d50b11c2641d
ICYrom <drewzamanigian@gmail.com>
2012-03-23 00:59:36 -0400

    updated product packages

--- full_crespo4g.mk
+++ full_crespo4g.mk
@@ -25,6 +25,8 @@ PRODUCT_PACKAGES := \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
+        LibertySettings \
+        Trebuchet \
         Galaxy4 \
         HoloSpiralWallpaper \
         LiveWallpapers \
